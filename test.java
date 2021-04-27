aafe5b83829b0809139070554cfaa026a34df638
Jangminjin <jangminjin227@gmail.com>
2021-04-27 11:11:31 +0900

    test_java

---
+++ test.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        // 프로그램 시작
+        // 한줄 주석
+        /*
+        여러줄 주석
+        */
+        // 화면 출력하는 방법
+        System.out.println("Hello Java");
+        // 프로그램 끝
+    }
+}
