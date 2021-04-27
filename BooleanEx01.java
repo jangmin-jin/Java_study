a75c579720e6423c9bbd21c8adf8405ed319334c
Jangminjin <jangminjin227@gmail.com>
2021-04-27 11:57:23 +0900

    main push test 2

---
+++ BooleanEx01.java
@@ -0,0 +1,16 @@
+public class BooleanEx01{
+    public static void main(String[] args){
+        // 변수의 선언
+        // 논리형
+        boolean b1;
+
+        // 초기화
+        b1 = true;
+
+        // 선언과 동시에 초기화
+        boolean b2 = false;
+
+        System.out.println( b1 );
+        System.out.println( b2 );
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        // 프로그램 시작
+        // 한줄 주석
+        /*
+        여러줄 주석
+        */
+        // 화면 출력하는 방법
+        System.out.println("Hello Java");
+        System.out.println("안녕 자바");
+
+        System.out.print("Hello Java");
+        System.out.print("안녕 자바");
+
+        // %s 문자열
+        // %d 숫자
+        System.out.printf("%s - %s - %s", "a", "b", "c");
+        // 프로그램 끝
+    }
+}
